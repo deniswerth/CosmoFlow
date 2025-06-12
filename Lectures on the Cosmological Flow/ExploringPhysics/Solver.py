449f894bd6482173e9de1a1e996775de91e6846f
Denis Werth <werth@iap.fr>
2025-06-12 17:42:01 +0200

    Add files via upload

---
+++ Lectures on the Cosmological Flow/ExploringPhysics/Solver.py
@@ -0,0 +1,442 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from scipy.integrate import odeint
+from scipy.interpolate import interp1d
+
+from Theory import theory
+
+class solver():
+	"""
+	This class defines the initial conditions and the flow equations, and contains the solver to numerically
+	integrate the complete set of equations
+	"""
+
+	def __init__(self, Nspan, Nfield, interpolated, Rtol, Atol):
+
+		self.Nspan = Nspan
+		self.Nfield = Nfield
+		self.interpolated = interpolated
+		self.N_init = self.Nspan[0]
+		self.Rtol = Rtol
+		self.Atol = Atol
+
+		#Defining class for initial conditions at N = N_init
+		self.initial = theory(self.N_init, self.Nfield, self.interpolated)
+
+	#Numerical solver for the 2- and 3-pt functions simultaneously
+	def Sigma_Re_init(self, k):
+		"""
+		Define the real part of the initial 2-pt function
+		"""
+		Nfield = self.Nfield
+		s = self.initial.scale
+		S = np.ones((Nfield, Nfield)) + (s-1)*np.eye(Nfield)
+		SigmaAB_Re_init = np.zeros((2*Nfield, 2*Nfield))
+		SigmaAB_Re_init[:Nfield, :Nfield] = self.initial.Delta_ab() * (1 + self.initial.H**2 * self.initial.a**2/k**2)
+		SigmaAB_Re_init[Nfield:, Nfield:] = k**2/self.initial.a**2 * np.diag(1/np.diagonal(self.initial.Delta_ab())) *S*S
+		SigmaAB_Re_init[Nfield:, :Nfield] = - self.initial.H * np.identity(Nfield) *S
+		SigmaAB_Re_init[:Nfield, Nfield:] = - self.initial.H * np.identity(Nfield) *S
+
+		#Speed of sound
+		theo = theory(N = self.Nspan, Nfield = self.Nfield, interpolated = self.interpolated)
+		cs = theo.cs_f(self.Nspan[0])
+		SigmaAB_Re_init[0, 0] = (1 + self.initial.H**2 * self.initial.a**2/k**2/cs**2)/cs
+		SigmaAB_Re_init[Nfield, Nfield] *= cs
+		SigmaAB_Re_init[0, Nfield] *= 1/cs
+		SigmaAB_Re_init[Nfield, 0] *= 1/cs
+
+		return 1/(2*k*self.initial.a**2) * SigmaAB_Re_init
+
+	def Sigma_Im_init(self, k):
+		"""
+		Define the imaginary part of the initial 2-pt function
+		"""
+		Nfield = self.Nfield
+		s = self.initial.scale
+		S = np.ones((Nfield, Nfield)) + (s-1)*np.eye(Nfield)
+		SigmaAB_Im_init = np.zeros((2*Nfield, 2*Nfield))
+		SigmaAB_Im_init[Nfield:, :Nfield] = - np.identity(Nfield) *S
+		SigmaAB_Im_init[:Nfield, Nfield:] = np.identity(Nfield) *S
+		return 1/(2 * self.initial.a**3) * SigmaAB_Im_init
+
+	def fffCalc(self, k1, k2, k3):
+		"""
+		Define the field-field-field initial conditions
+		"""
+		Nfield = self.Nfield
+		a = self.initial.a
+		Hi = self.initial.H
+
+		kt = k1 + k2 + k3
+		K2 = k1*k2 + k1*k3 + k2*k3
+
+		Af123 = self.initial.A_abc_fast(k1, k2, k3)
+		Af132 = self.initial.A_abc_fast(k1, k3, k2)
+		Af231 = self.initial.A_abc_fast(k2, k3, k1)
+
+		As123 = self.initial.A_abc_slow(k1, k2, k3)
+		As132 = self.initial.A_abc_slow(k1, k3, k2)
+		As231 = self.initial.A_abc_slow(k2, k3, k1)
+
+		B123 = self.initial.B_abc(k1, k2, k3)
+		B132 = self.initial.B_abc(k1, k3, k2)
+		B231 = self.initial.B_abc(k2, k3, k1)
+
+		C123 = self.initial.C_abc(k1, k2, k3)
+		C132 = self.initial.C_abc(k1, k3, k2)
+		C231 = self.initial.C_abc(k2, k3, k1)
+
+		D123 = self.initial.D_abc(k1, k2, k3)
+		D132 = self.initial.D_abc(k1, k3, k2)
+		D231 = self.initial.D_abc(k2, k3, k1)
+
+		fff = np.zeros((Nfield, Nfield, Nfield))
+
+		for i in range(Nfield):
+			for j in range(Nfield):
+				for k in range(Nfield):
+					fff[i, j, k] += 1/(a*a*a*a)/4/(k1*k2*k3)/kt*(-C123[i, j, k]*k1*k2 - C132[i, k, j]*k1*k3 - C231[j, k, i]*k2*k3
+													+ a*a*As123[i, j, k] + a*a*As132[i, k, j] + a*a*As231[j, k, i]
+													+ a*a*Hi*B123[i, j, k]*((k1+k2)*k3/k1/k2 - K2/k1/k2)
+													+ a*a*Hi*B132[i, k, j]*((k1+k3)*k1/k1/k3 - K2/k1/k3)
+													+ a*a*Hi*B231[j, k, i]*((k2+k3)*k1/k2/k3 - K2/k2/k3)
+													+ D123[i, j, k]*Hi*(K2 - 2*k1*k2*k2/kt)
+													+ D132[i, k, j]*Hi*(K2 - 2*k1*k2*k2/kt)
+													+ D231[j, k, i]*Hi*(K2 - 2*k1*k2*k2/kt)
+													)
+					fff[i, j, k] += 1./(a*a*a*a)/4./(k1*k2*k3)/kt * (Af123[i, j, k] + Af132[i, k, j] + Af231[j, k, i])
+
+		return fff
+
+	def pffCalc(self, k1, k2, k3):
+		"""
+		Define the momentum-field-field initial conditions
+		"""
+		Nfield = self.Nfield
+		a = self.initial.a
+		Hi = self.initial.H
+
+		kt = k1 + k2 + k3
+		K2 = k1*k2 + k1*k3 + k2*k3
+		K3 = k1*k1*k1 * k2*k2*k2 * k3*k3*k3
+
+		Af123 = self.initial.A_abc_fast(k1, k2, k3)
+		Af132 = self.initial.A_abc_fast(k1, k3, k2)
+		Af231 = self.initial.A_abc_fast(k2, k3, k1)
+
+		As123 = self.initial.A_abc_slow(k1, k2, k3)
+		As132 = self.initial.A_abc_slow(k1, k3, k2)
+		As231 = self.initial.A_abc_slow(k2, k3, k1)
+
+		B123 = self.initial.B_abc(k1, k2, k3)
+		B132 = self.initial.B_abc(k1, k3, k2)
+		B231 = self.initial.B_abc(k2, k3, k1)
+
+		C123 = self.initial.C_abc(k1, k2, k3)
+		C132 = self.initial.C_abc(k1, k3, k2)
+		C231 = self.initial.C_abc(k2, k3, k1)
+
+		D123 = self.initial.D_abc(k1, k2, k3)
+		D132 = self.initial.D_abc(k1, k3, k2)
+		D231 = self.initial.D_abc(k2, k3, k1)
+
+		pff = np.zeros((Nfield, Nfield, Nfield))
+		
+		for i in range(Nfield):
+			for j in range(Nfield):
+				for k in range(Nfield):
+					pff[i, j, k] += - 1./(a*a*a)/4./K3 * Hi * (-k1*k1*(k2+k3)/kt* k1*k2*k3) * (-C123[i, j, k]*k1*k2 - C132[i, k, j]*k1*k3 - C231[j, k, i]*k2*k3
+		 																			+ a*a*As123[i, j, k] + a*a*As132[i, k, j] + a*a*As231[j, k, i]
+		 																			-2*D123[i, j, k]*Hi*k1*k2*k3/kt - 2*D132[i, k, j]*Hi*k1*k2*k3/kt - 2*D231[j, k, i]*Hi*k1*k2*k3/kt
+		 																			)
+					pff[i, j, k] += - 1./(a*a*a)/4./K3 * Hi * (-k1*k1*(k2+k3)/kt* k1*k2*k3) * (Af123[i, j, k] + Af132[i, k, j] + Af231[j, k, i])
+					pff[i, j, k] += - 1./(a*a*a)/4./K3 * Hi * (-k1*k1*k2*k3/kt) * (C123[i, j, k]*k1*k1*k2*k2*(1.+k3/kt)
+																	+ C132[i, k, j]*k1*k1*k3*k3*(1.+k2/kt)
+																	+ C231[j, k, i]*k3*k3*k2*k2*(1.+k1/kt)
+																	- a*a*As123[i, j, k]*(K2 - k1*k2*k3/kt)
+																	- a*a*As132[i, k, j]*(K2 - k1*k2*k3/kt)
+																	- a*a*As231[j, k, i]*(K2 - k1*k2*k3/kt)
+																	- D123[i, j, k]*(k1*k2*k3)**2/Hi/a/a
+																	- D132[i, k, j]*(k1*k2*k3)**2/Hi/a/a
+																	- D231[j, k, i]*(k1*k2*k3)**2/Hi/a/a
+																	)
+					pff[i, j, k] += - 1./(a*a*a)/4./K3 * Hi * (-k1*k1*k2*k3/kt) * (B123[i, j, k]/Hi*k1*k2*k3*k3
+																	+ B132[i, k, j]/Hi*k1*k3*k2*k2
+																	+ B231[j, k, i]/Hi*k2*k3*k1*k1
+																	)
+					pff[i, j, k] += - 1./(a*a*a)/4./K3 * Hi * (-k1*k1*k2*k3/kt) * (-Af123[i, j, k]*(K2 +k1*k2*k3/kt) - Af132[i, k, j]*(K2 +k1*k2*k3/kt) - Af231[j, k, i]*(K2 +k1*k2*k3/kt))
+
+		return pff
+
+
+	def ppfCalc(self, k1, k2, k3):
+		"""
+		Define the momentum-momentum-field initial conditions 
+		"""
+		Nfield = self.Nfield
+		a = self.initial.a
+		Hi = self.initial.H
+
+		kt = k1 + k2 + k3
+		K3 = k1*k1*k1 * k2*k2*k2 * k3*k3*k3
+
+		Af123 = self.initial.A_abc_fast(k1, k2, k3)
+		Af132 = self.initial.A_abc_fast(k1, k3, k2)
+		Af231 = self.initial.A_abc_fast(k2, k3, k1)
+
+		As123 = self.initial.A_abc_slow(k1, k2, k3)
+		As132 = self.initial.A_abc_slow(k1, k3, k2)
+		As231 = self.initial.A_abc_slow(k2, k3, k1)
+
+		B123 = self.initial.B_abc(k1, k2, k3)
+		B132 = self.initial.B_abc(k1, k3, k2)
+		B231 = self.initial.B_abc(k2, k3, k1)
+
+		C123 = self.initial.C_abc(k1, k2, k3)
+		C132 = self.initial.C_abc(k1, k3, k2)
+		C231 = self.initial.C_abc(k2, k3, k1)
+
+		D123 = self.initial.D_abc(k1, k2, k3)
+		D132 = self.initial.D_abc(k1, k3, k2)
+		D231 = self.initial.D_abc(k2, k3, k1)
+
+		ppf = np.zeros((Nfield, Nfield, Nfield))
+		
+		for i in range(Nfield):
+			for j in range(Nfield):
+				for k in range(Nfield):
+					ppf[i, j, k] += -1./(a*a*a*a)/4./K3 * (k1*k2*k3)*(k1*k2*k3)/kt*k1*k2*(-C123[i, j, k]*k1*k2 - C132[i, k, j]*k1*k3 - C231[j, k, i]*k2*k3
+																	+ a*a*As123[i, j, k] + a*a*As132[i, k, j] + a*a*As231[j, k, i]
+																	+ a*a*Hi*B123[i, j, k]*(k1+k2)*k3/k1/k2
+																	+ a*a*Hi*B132[i, k, j]*(k1+k3)*k2/k1/k3
+																	+ a*a*Hi*B231[j, k, i]*(k2+k3)*k1/k2/k3
+																	+ a*a*Hi*B123[i, j, k]*k3*k3/k2/k2
+																	+ a*a*Hi*B132[i, k, j]*k2*k2/k1/k1
+																	+ a*a*Hi*B231[j, k, i]*k1*k1/k3/k3
+																	- 2*D123[i, j, k]*Hi*k1*k2*k3/kt
+																	- 2*D132[i, k, j]*Hi*k1*k2*k3/kt
+																	- 2*D123[j, k, i]*Hi*k1*k2*k3/kt
+																	)
+					ppf[i, j, k] += - 1./(a*a*a*a)/4/K3 * (k1*k2*k3)*(k1*k2*k3)/kt*k1*k2 * (Af123[i, j, k] + Af132[i, k, j] + Af231[j, k, i])
+		
+		return ppf
+
+	def pppCalc(self, k1, k2, k3):
+		"""
+		Define the momentum-momentum-momentum initial conditions 
+		"""
+		Nfield = self.Nfield
+		a = self.initial.a
+		Hi = self.initial.H
+
+		kt = k1 + k2 + k3
+		K2 = k1*k2 + k1*k3 + k2*k3
+		K3 = k1*k1*k1 * k2*k2*k2 * k3*k3*k3
+
+		Af123 = self.initial.A_abc_fast(k1, k2, k3)
+		Af132 = self.initial.A_abc_fast(k1, k3, k2)
+		Af231 = self.initial.A_abc_fast(k2, k3, k1)
+
+		As123 = self.initial.A_abc_slow(k1, k2, k3)
+		As132 = self.initial.A_abc_slow(k1, k3, k2)
+		As231 = self.initial.A_abc_slow(k2, k3, k1)
+
+		B123 = self.initial.B_abc(k1, k2, k3)
+		B132 = self.initial.B_abc(k1, k3, k2)
+		B231 = self.initial.B_abc(k2, k3, k1)
+
+		C123 = self.initial.C_abc(k1, k2, k3)
+		C132 = self.initial.C_abc(k1, k3, k2)
+		C231 = self.initial.C_abc(k2, k3, k1)
+
+		D123 = self.initial.D_abc(k1, k2, k3)
+		D132 = self.initial.D_abc(k1, k3, k2)
+		D231 = self.initial.D_abc(k2, k3, k1)
+		
+		ppp = np.zeros((Nfield, Nfield, Nfield))
+		
+		for i in range(Nfield):
+			for j in range(Nfield):
+				for k in range(Nfield):
+					ppp[i, j, k] += - 1./(a*a*a)/4./K3 * Hi * (k1*k1*k2*k2*k3*k3)/kt  * (C123[i, j, k]*k1*k1*k2*k2*(1.+k3/kt)
+																+ C132[i, k, j]*k1*k1*k3*k3*(1.+k2/kt)
+																+ C231[j, k, i]*k3*k3*k2*k2*(1.+k1/kt)
+																- a*a*As123[i, j, k]*(K2 - k1*k2*k3/kt)
+																- a*a*As132[i, k, j]*(K2 - k1*k2*k3/kt)
+																- a*a*As231[j, k, i]*(K2 - k1*k2*k3/kt)
+																- D123[i, j, k]*(k1*k2*k3)**2/Hi/a/a
+																- D132[i, k, j]*(k1*k2*k3)**2/Hi/a/a
+																- D231[j, k, i]*(k1*k2*k3)**2/Hi/a/a
+																)
+					ppp[i, j, k] += - 1./(a*a*a)/4./K3 * Hi * (k1*k1*k2*k2*k3*k3)/kt * (B123[i, j, k]/Hi*k1*k2*k3*k3
+																+ B132[i, k, j]/Hi*k1*k3*k2*k2
+																+ B231[j, k, i]/Hi*k2*k3*k1*k1
+																)
+					ppp[i, j, k] += - 1./(a*a*a)/4./K3 * Hi * (k1*k1*k2*k2*k3*k3)/kt * (-Af123[i, j, k]*(K2 +k1*k2*k3/kt) - Af132[i, k, j]*(K2 +k1*k2*k3/kt) - Af231[j, k, i]*(K2 +k1*k2*k3/kt))
+
+		return ppp
+
+	def B_init(self, k1, k2, k3):
+		"""
+		Define the real part of the initial 3-pt functions
+		"""
+		Nfield = self.Nfield
+		a = self.initial.a
+		H = self.initial.H
+		s = self.initial.scale
+		S = np.ones((Nfield, Nfield, Nfield)) + (s-1)*np.eye(Nfield)
+
+		B = np.zeros((2*Nfield, 2*Nfield, 2*Nfield))
+
+		fff = self.fffCalc(k1, k2, k3)
+		pff = self.pffCalc(k1, k2, k3)
+		fpf = self.pffCalc(k2, k1, k3)
+		ffp = self.pffCalc(k3, k1, k2)
+		ppf = self.ppfCalc(k1, k2, k3)
+		pfp = self.ppfCalc(k1, k3, k2)
+		fpp = self.ppfCalc(k2, k3, k1)
+		ppp = self.pppCalc(k1, k2, k3)
+
+		for i in range(Nfield):
+			for j in range(Nfield):
+				for k in range(Nfield):
+					B[i, j, k] = fff[i, j, k]
+					B[Nfield+i, j, k] = pff[i, j, k]/a   *s
+					B[i, Nfield+j, k] = fpf[j, i, k]/a   *s
+					B[i, j, Nfield+k] = ffp[k, i, j]/a   *s
+					B[i, Nfield+j, Nfield+k] = fpp[j, k, i]/a/a  *s*s
+					B[Nfield+i, Nfield+j, k] = ppf[i, j, k]/a/a  *s*s
+					B[Nfield+i, j,  Nfield+k] = pfp[i, k, j]/a/a   *s*s
+					B[Nfield+i, Nfield+j,  Nfield+k] = ppp[i, j, k]/a/a/a  *s*s*s
+
+		return B
+
+
+	def f_init(self, k1, k2, k3):
+		"""
+		Define the initial conditions for 2- and 3-pt functions
+		"""
+		Nfield = self.Nfield
+		return np.concatenate((np.reshape(self.Sigma_Re_init(k1), 2*Nfield*2*Nfield), np.reshape(self.Sigma_Re_init(k2), 2*Nfield*2*Nfield), np.reshape(self.Sigma_Re_init(k3), 2*Nfield*2*Nfield), 
+						np.reshape(self.Sigma_Im_init(k1), 2*Nfield*2*Nfield), np.reshape(self.Sigma_Im_init(k2), 2*Nfield*2*Nfield), np.reshape(self.Sigma_Im_init(k3), 2*Nfield*2*Nfield),
+						np.reshape(self.B_init(k1, k2, k3), 2*Nfield*2*Nfield*2*Nfield)))
+
+
+	def df_dN(self, y, N, k1, k2, k3):
+		"""
+		Define the flow equation for the 2- and 3-pt functions in a flatten way
+		y is a flat array of dimension (2*Nfield*2*Nfield * 2*Nfield*2*Nfield * 2*Nfield*2*Nfield  * 2*Nfield*2*Nfield * 2*Nfield*2*Nfield * 2*Nfield*2*Nfield  *  2Nfield*2Nfield*2Nfield)
+		for (Sigma_Re_k1, Sigma_Re_k2, Sigma_Re_k3, Sigma_Im_k1, Sigma_Im_k2, Sigma_Im_k2, B_Re)
+		"""
+		Nfield = self.Nfield
+
+		n = 2*Nfield*2*Nfield
+		Sigma_Re_k1 = np.reshape(y[:n], (2*Nfield, 2*Nfield))
+		Sigma_Re_k2 = np.reshape(y[n:n*2], (2*Nfield, 2*Nfield))
+		Sigma_Re_k3 = np.reshape(y[n*2:n*3], (2*Nfield, 2*Nfield))
+		Sigma_Im_k1 = np.reshape(y[n*3:n*4], (2*Nfield, 2*Nfield))
+		Sigma_Im_k2 = np.reshape(y[n*4:n*5], (2*Nfield, 2*Nfield))
+		Sigma_Im_k3 = np.reshape(y[n*5:n*6], (2*Nfield, 2*Nfield))
+		B = np.reshape(y[n*6:], (2*Nfield, 2*Nfield, 2*Nfield))
+	
+		mdl = theory(N = N, Nfield = self.Nfield, interpolated = self.interpolated)
+		uAB_k1 = mdl.u_AB(k1)
+		uAB_k2 = mdl.u_AB(k2)
+		uAB_k3 = mdl.u_AB(k3)
+		uABC_k1k2k3 = mdl.u_ABC(k1, k2, k3)
+		uABC_k2k1k3 = mdl.u_ABC(k2, k1, k3)
+		uABC_k3k1k2 = mdl.u_ABC(k3, k1, k2)
+
+
+		#2-pt flow equations
+		#k1 (Re)
+		dSig1 = np.dot(uAB_k1, Sigma_Re_k1)
+		dSig2 = np.einsum("bc, ac -> ab", uAB_k1, Sigma_Re_k1)
+		dSig_dN_Re_k1 = dSig1 + dSig2
+		#k2 (Re)
+		dSig1 = np.dot(uAB_k2, Sigma_Re_k2)
+		dSig2 = np.einsum("bc, ac -> ab", uAB_k2, Sigma_Re_k2)
+		dSig_dN_Re_k2 = dSig1 + dSig2
+		#k3 (Re)
+		dSig1 = np.dot(uAB_k3, Sigma_Re_k3)
+		dSig2 = np.einsum("bc, ac -> ab", uAB_k3, Sigma_Re_k3)
+		dSig_dN_Re_k3 = dSig1 + dSig2
+
+		#k1 (Im)
+		dSig1 = np.dot(uAB_k1, Sigma_Im_k1)
+		dSig2 = np.einsum("bc, ac -> ab", uAB_k1, Sigma_Im_k1)
+		dSig_dN_Im_k1 = dSig1 + dSig2
+		#k2 (Im)
+		dSig1 = np.dot(uAB_k2, Sigma_Im_k2)
+		dSig2 = np.einsum("bc, ac -> ab", uAB_k2, Sigma_Im_k2)
+		dSig_dN_Im_k2 = dSig1 + dSig2
+		#k3 (Im)
+		dSig1 = np.dot(uAB_k3, Sigma_Im_k3)
+		dSig2 = np.einsum("bc, ac -> ab", uAB_k3, Sigma_Im_k3)
+		dSig_dN_Im_k3 = dSig1 + dSig2
+		#Impose the imaginary part to be zero because of the commutation relation (except field and its conjugate momentum)
+		for i in range(2*Nfield):
+				for j in range(2*Nfield):
+					if i != Nfield + j and j != Nfield + i:
+						dSig_dN_Im_k1[i, j] = 0
+						dSig_dN_Im_k2[i, j] = 0
+						dSig_dN_Im_k3[i, j] = 0
+		
+		dSig_dN_Re_k1 = np.reshape(dSig_dN_Re_k1, (2*Nfield)**2)
+		dSig_dN_Re_k2 = np.reshape(dSig_dN_Re_k2, (2*Nfield)**2)
+		dSig_dN_Re_k3 = np.reshape(dSig_dN_Re_k3, (2*Nfield)**2)
+		dSig_dN_Im_k1 = np.reshape(dSig_dN_Im_k1, (2*Nfield)**2)
+		dSig_dN_Im_k2 = np.reshape(dSig_dN_Im_k2, (2*Nfield)**2)
+		dSig_dN_Im_k3 = np.reshape(dSig_dN_Im_k3, (2*Nfield)**2)
+		
+		#3-pt flow equations
+		dB1 = np.einsum("ad, dbc -> abc", uAB_k1, B)
+		dB11 = np.einsum("ade, db, ec -> abc", uABC_k1k2k3, Sigma_Re_k2, Sigma_Re_k3)
+		dB22 = np.einsum("ade, db, ec -> abc", uABC_k1k2k3, Sigma_Im_k2, Sigma_Im_k3)
+
+		dB2 = np.einsum("bd, adc -> abc", uAB_k2, B)
+		dB33 = np.einsum("bde, ad, ec -> abc", uABC_k2k1k3, Sigma_Re_k1, Sigma_Re_k3)
+		dB44 = np.einsum("bde, ad, ec -> abc", uABC_k2k1k3, Sigma_Im_k1, Sigma_Im_k3)
+
+		dB3 = np.einsum("cd, abd -> abc", uAB_k3, B)
+		dB55 = np.einsum("cde, ad, be -> abc", uABC_k3k1k2, Sigma_Re_k1, Sigma_Re_k2)
+		dB66 = np.einsum("cde, ad, be -> abc", uABC_k3k1k2, Sigma_Im_k1, Sigma_Im_k2)
+
+		dB_dN = np.reshape(dB1 + dB2 + dB3 + dB11 - dB22 + dB33 - dB44 + dB55 - dB66, (2*Nfield)**3)
+
+		#Concatenate all differential equations into a flatten array (right format for the solver)
+		df_dN = np.concatenate((dSig_dN_Re_k1, dSig_dN_Re_k2, dSig_dN_Re_k3, dSig_dN_Im_k1, dSig_dN_Im_k2, dSig_dN_Im_k3, dB_dN))
+		
+		return df_dN
+
+	def f_solution(self, k1, k2, k3):
+		"""
+		Solve for 2- and 3-pt functions given k1, k2, k3 and a tensor of initial conditions
+		"""
+		rtol, atol = self.Rtol, self.Atol
+		Nfield = self.Nfield
+		Nspan = self.Nspan
+
+		f0 = self.f_init(k1, k2, k3)
+		dimension = 2*Nfield*2*Nfield * 2*Nfield*2*Nfield * 2*Nfield*2*Nfield  * 2*Nfield*2*Nfield * 2*Nfield*2*Nfield * 2*Nfield*2*Nfield  *  2*Nfield*2*Nfield*2*Nfield
+		N_boundary = [Nspan[0], Nspan[-1]]
+
+		sol = solve_ivp(lambda N, y: self.df_dN(y, N, k1, k2, k3), N_boundary, f0, t_eval = Nspan, dense_output = True, method = "RK45", rtol = rtol, atol = atol)
+		f = sol.y
+
+		n = 2*Nfield*2*Nfield
+		Nl = len(Nspan)
+		Sigma_Re_k1 = np.reshape(f[:n], (2*Nfield, 2*Nfield, Nl))
+		Sigma_Re_k2 = np.reshape(f[n:n*2], (2*Nfield, 2*Nfield, Nl))
+		Sigma_Re_k3 = np.reshape(f[n*2:n*3], (2*Nfield, 2*Nfield, Nl))
+		Sigma_Im_k1 = np.reshape(f[n*3:n*4], (2*Nfield, 2*Nfield, Nl))
+		Sigma_Im_k2 = np.reshape(f[n*4:n*5], (2*Nfield, 2*Nfield, Nl))
+		Sigma_Im_k3 = np.reshape(f[n*5:n*6], (2*Nfield, 2*Nfield, Nl))
+		B           = np.reshape(f[n*6:], (2*Nfield, 2*Nfield, 2*Nfield, Nl))
+
+		f = [Sigma_Re_k1, Sigma_Re_k2, Sigma_Re_k3, Sigma_Im_k1,Sigma_Im_k2, Sigma_Im_k3, B]
+
+		return f
+
+
---
+++ Lectures on the Cosmological Flow/ExploringPhysics/Theory.py
@@ -0,0 +1,197 @@
+import numpy as np # import Numpy package for vectorisation
+from scipy.misc import derivative # import derivative from Scipy package
+
+
+class theory():
+	"""
+	This class defines the theory: (i) time-dependent functions, (ii) tensors Delta, M, I, A, B, C and D, and (iii) tensors u^A_B and u^A_BC
+	All functions have been optimised to be computed once at each integration step.
+	"""
+
+	def __init__(self, N, Nfield, interpolated):
+		self.N = N
+		self.Nfield = Nfield
+		self.interpolated = interpolated # (H_f, cs_f, m_f, rho_f, g_f)
+
+		#Evaluate the parameters at N
+		N = self.N
+		self.H = self.H_f(N)
+		self.cs = self.cs_f(N)
+		self.m = self.m_f(N)
+		self.rho = self.rho_f(N)
+		self.g = self.g_f(N)
+
+		#Additional functions and scale functions
+		self.a = self.a_f(N) # Scale factor
+		self.dH = self.dH_f(N) # Derivative of Hubble
+		self.scale = self.scale_f(N) # Rescale function to improve performances
+		self.dscale = self.dscale_f(N) # Derivative of rescale function
+
+
+	#Define continuous functions
+	def H_f(self, N):
+		return self.interpolated[0](N)
+
+	def cs_f(self, N):
+		return self.interpolated[1](N)
+
+	def m_f(self, N):
+		return self.interpolated[2](N)
+
+	def rho_f(self, N):
+		return self.interpolated[3](N)
+
+	def g_f(self, N):
+		return self.interpolated[4](N)
+
+
+	#Deduced additional functions
+	def a_f(self, N):
+		"""
+		Scale factor as a function of the number of e-folds
+		"""
+		return np.exp(N)
+
+	def dH_f(self, N):
+		"""
+		Derivative of the Hubble rate (with respect to cosmic time)
+		"""
+		dHdN = derivative(self.H_f, N, dx = 1e-6)
+		return self.H_f(N)*dHdN
+
+	def k_mode(self, N_exit):
+		"""
+		Return the mode k horizon crossing corresponding to N_exit
+		"""
+		return self.a_f(N_exit) * self.H_f(N_exit)
+
+	def scale_f(self, N):
+		"""
+		Define the rescaled scale factor to improve performance
+		"""
+		k = 1
+		a = self.a_f(N)
+		H = self.H_f(N)
+		return a/(1. + a*H/k)/H
+
+	def dscale_f(self, N):
+		"""
+		Define the derivative of scale function
+		"""
+		k = 1
+		a = self.a_f(N)
+		H = self.H_f(N)
+		Hd = self.dH_f(N)
+		return -Hd/H/H*a/(1. + a*H/k) + a/(1. + a*H/k) - a*(a*H*H/k + a*Hd/k)/(1. + a*H/k)/(1. + a*H/k)/H
+
+
+	# Define the quadratic theory tensors
+	def Delta_ab(self):
+		Nfield = self.Nfield
+		Deltaab = np.eye(Nfield) # Identity matrix of size Nfield.Nfield
+		return Deltaab
+
+	def I_ab(self):
+		Nfield = self.Nfield
+		Iab = np.zeros((Nfield, Nfield))
+		Iab[0, 1] = self.rho
+		return Iab
+
+	def M_ab(self, k):
+		Nfield = self.Nfield
+		Mab = np.eye(Nfield)
+		Mab[0, 0] = -k**2/self.a**2 * self.cs**2
+		Mab[1, 1] = -k**2/self.a**2 - self.m**2 - self.rho**2
+		return Mab
+
+
+	# Define the cubic theory tensors
+	def A_abc(self, k1, k2, k3):
+		Nfield = self.Nfield
+		Aabc = np.zeros((Nfield, Nfield, Nfield))
+		k1k2 = (k3**2 - k1**2 - k2**2)/2
+		k1k3 = (k2**2 - k1**2 - k3**2)/2
+		k2k3 = (k1**2 - k2**2 - k3**2)/2
+		Aabc[0, 0, 1] += self.g * k1k2/self.a**2 /3
+		Aabc[0, 1, 0] += self.g * k1k3/self.a**2 /3
+		Aabc[1, 0, 0] += self.g * k2k3/self.a**2 /3
+		return Aabc
+
+	def A_abc_fast(self, k1, k2, k3): # For initial conditions
+		Nfield = self.Nfield
+		Aabc = np.zeros((Nfield, Nfield, Nfield))
+		k1k2 = (k3**2 - k1**2 - k2**2)/2
+		k1k3 = (k2**2 - k1**2 - k3**2)/2
+		k2k3 = (k1**2 - k2**2 - k3**2)/2
+		Aabc[0, 0, 1] += self.g * k1k2 /3
+		Aabc[0, 1, 0] += self.g * k1k3 /3
+		Aabc[1, 0, 0] += self.g * k2k3 /3
+		return Aabc
+
+	def A_abc_slow(self, k1, k2, k3):
+		Nfield = self.Nfield
+		Aabc = np.zeros((Nfield, Nfield, Nfield))
+		return Aabc
+
+	def B_abc(self, k1, k2, k3):
+		Nfield = self.Nfield
+		Babc = np.zeros((Nfield, Nfield, Nfield))
+		return Babc
+
+	def C_abc(self, k1, k2, k3):
+		Nfield = self.Nfield
+		Cabc = np.zeros((Nfield, Nfield, Nfield))
+		return Cabc
+
+	def D_abc(self, k1, k2, k3):
+		Nfield = self.Nfield
+		Dabc = np.zeros((Nfield, Nfield, Nfield))
+		return Dabc
+
+
+	# Define the u-tensors
+	def u_AB(self, k):
+		Nfield = self.Nfield
+		H = self.H
+		s = self.scale
+		ds = self.dscale
+		S = np.ones((Nfield, Nfield)) + (s-1)*np.eye(Nfield)
+		uAB = np.zeros((2*Nfield, 2*Nfield))
+		uAB[:Nfield, :Nfield] = -self.I_ab()/H
+		uAB[:Nfield, Nfield:] = self.Delta_ab()/H /s
+		uAB[Nfield:, :Nfield] = self.M_ab(k)/H *s
+		uAB[Nfield:, Nfield:] = (self.I_ab()).T/H - 3*self.H*np.eye(Nfield)/H + ds/s*np.eye(Nfield)/H 
+		return uAB
+
+	def u_ABC(self, k1, k2, k3):
+		Nfield = self.Nfield
+		s = self.scale
+		S = np.ones((Nfield, Nfield, Nfield)) + (s-1)*np.eye(Nfield)
+		H = self.H
+		uABC = np.zeros((2*Nfield, 2*Nfield, 2*Nfield))
+
+		A123 = self.A_abc(k1, k2, k3)
+		B123 = self.B_abc(k1, k2, k3)
+		B132 = self.B_abc(k1, k3, k2)
+		B231 = self.B_abc(k2, k3, k1)
+		C123 = self.C_abc(k1, k2, k3)
+		C132 = self.C_abc(k1, k3, k2)
+		C321 = self.C_abc(k2, k3, k1)
+		D123 = self.D_abc(k1, k2, k3)
+
+		for i in range(Nfield):
+			for j in range(Nfield):
+				for k in range(Nfield):
+					uABC[i, j, k] = -B231[j, k, i]/H
+					uABC[i, Nfield+j, k] = -C123[i, j, k]/H/s
+					uABC[i, j, Nfield+k] = -C132[i, k, j]/H/s
+					uABC[Nfield+i, Nfield+j, Nfield+k] = C321[k, j, i]/H/s
+					uABC[i, Nfield+j, Nfield+k] = 3.*D123[i, j, k]/H/s/s
+					uABC[Nfield+i, j, k] = 3.*A123[i, j, k]/H*s
+					uABC[Nfield+i, Nfield+j, k] = B132[i, k, j]/H
+					uABC[Nfield+i, j, Nfield+k] = B123[i, j, k]/H
+		return uABC
+
+
+
+
